1873899ce869bccec48a72838157a429e18c65b7
Sarif167 <wajsarif461@gmail.com>
2025-06-06 10:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,9 +53,9 @@ FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b
 
 
 API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+URL = environ.get("URL", "onylinks.com") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/howtoopenlink5/13") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "SouravSearch_bot") # bot username without @
 VERIFY = environ.get("VERIFY", "True")
 
 
